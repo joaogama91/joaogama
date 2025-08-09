6ae28804170d25f1acffaa5fbe89e3a8a5ad1f1d
joaogama91 <19joaogama91@gmail.com>
2025-08-09 02:00:12 +0100

    Publicar build para domínio joaogama.art (base=/, CNAME e 404)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/joaogama/',   // <- nome do repositório no GitHub
+  base: '/',          // <-- MUITO IMPORTANTE (antes era '/joaogama/')
   plugins: [react()],
 })
